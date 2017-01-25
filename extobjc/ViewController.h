bb18317590fecedc2e9cb558774d1a0dcf822f5a
Silence <374619540@qq.com>
2017-01-25 17:54:14 +0800

    Add source

---
+++ extobjc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  extobjc
+//
+//  Created by 杨晴贺 on 25/01/2017.
+//  Copyright © 2017 Silence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ extobjc/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  extobjc
+//
+//  Created by 杨晴贺 on 25/01/2017.
+//  Copyright © 2017 Silence. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+
+@end
